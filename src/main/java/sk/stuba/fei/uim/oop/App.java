af8e329fd1d090af6a1b2f7965384b872d9415c2
Jakub Kovář <jakub.kovar@stuba.sk>
2021-02-16 09:23:10 +0100

    Delenie

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -2,17 +2,14 @@ package sk.stuba.fei.uim.oop;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-
-        int celeCislo = 5;
-        long celeCisloVacsie = 5L;
-        float desatinneSinglePrecision = 5.5f;
-        double desatinneDoublePrecision = 5.5;
-        char znak = 'a';
-        String retazec = "a";
 
         int podiel = 2/3; // 0
-        int poDeleni = 2%3; // 2
+        System.out.println(podiel);
 
+        int poDeleni = 2%3; // 2
+        System.out.println(poDeleni);
+
+        float podielDesatinny = 2/3.0f;
+        System.out.println(podielDesatinny);
     }
 }

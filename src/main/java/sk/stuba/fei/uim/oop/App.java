fe2736b491b215ce9556ebd658385e365b25e28b
Jakub Kovář <jakub.kovar@stuba.sk>
2021-02-16 09:24:27 +0100

    Boolean

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -2,14 +2,7 @@ package sk.stuba.fei.uim.oop;
 
 public class App {
     public static void main(String[] args) {
-
-        int podiel = 2/3; // 0
-        System.out.println(podiel);
-
-        int poDeleni = 2%3; // 2
-        System.out.println(poDeleni);
-
-        float podielDesatinny = 2/3.0f;
-        System.out.println(podielDesatinny);
+        boolean pravda = true;
+        boolean nepravda = false;
     }
 }

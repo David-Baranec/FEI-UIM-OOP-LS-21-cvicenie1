81a05cb147370ab0d08175b709f1680cc6a41b83
Jakub Kovář <jakub.kovar@stuba.sk>
2021-02-16 09:33:53 +0100

    for a switch

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -2,10 +2,21 @@ package sk.stuba.fei.uim.oop;
 
 public class App {
     public static void main(String[] args) {
-        if (false)
-            System.out.print("Hello ");
-        else if (true) {
-            System.out.println("world");
+        for (int a = 0; a < 6; a++) {
+            switch (a) {
+                case 1:
+                    System.out.println("a je jedna");
+                    break;
+                case 2:
+                    System.out.println("a je dva");
+                    break;
+                case 3:
+                case 4:
+                    System.out.println("a je 3 alebo 4");
+                    break;
+                default:
+                    System.out.println(String.format("a je %d", a));
+            }
         }
     }
 }
